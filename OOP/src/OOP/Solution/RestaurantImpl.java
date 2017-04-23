420a0043444cf9d77d8a43f1c507857a9f7eb468
tzeran <serantz@campus.technion.ac.il>
2017-04-23 19:58:28 +0300

    implementing RestaurantImpl

--- OOP/src/OOP/Solution/RestaurantImpl.java
+++ OOP/src/OOP/Solution/RestaurantImpl.java
@@ -3,51 +3,80 @@ package OOP.Solution;
 import OOP.Provided.HungryStudent;
 import OOP.Provided.Restaurant;
 
-import java.util.Set;
+import java.util.*;
 
-/**
- * Created by ashiber on 21-Apr-17.
- */
+
 public class RestaurantImpl implements Restaurant {
 
     private int id;
     private String name;
     private int distance;
     private Set<String> menu;
-
+    /**
+     * a list of all the students that rates the restaurant
+     */
+    HashMap<HungryStudent, Integer> students ;
 
     public RestaurantImpl(int id, String name, int distance, Set<String> menu) {
         this.id = id;
         this.name = name;
         this.distance = distance;
         this.menu = menu;
+        this.students = new HashMap<HungryStudent, Integer>();
     }
-
 
 
     @Override
     public int distance() {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        return this.distance;
     }
 
     @Override
     public Restaurant rate(HungryStudent s, int r) throws RateRangeException {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        HungryStudent temp;
+        if(r < 0 || r > 5) throw new RateRangeException();
+        this.students.put(s,r);
+        return this;
     }
 
     @Override
     public int numberOfRates() {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        return this.students.size();
     }
 
     @Override
-    public double averageRating() {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    public double averageRating(){
+        double ret=0;
+        if(this.students.size() != 0) {
+            for(Integer value : this.students.values()) {
+                ret += value;
+            }
+        }
+        return ret/((double)this.students.size());
     }
 
     @Override
     public int compareTo(Restaurant o) {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        if (this.equals(o))
+            return 0;
+        if (o instanceof RestaurantImpl) {
+            RestaurantImpl other = (RestaurantImpl) o;
+            if (this.getId() < other.getId())
+                return -1;
+            return 1;
+        }
+        //TODO: what should be returned here?
+        throw new RuntimeException("o is not RestaurantImpl instance");
+    }
+
+    /**
+     * the id of the Restaurant.
+     *
+     * @return the hash code of a student
+     */
+    @Override
+    public int hashCode() {
+        return id;
     }
 
     /**
@@ -56,7 +85,7 @@ public class RestaurantImpl implements Restaurant {
      * @return true if it was rated, false otherwise.
      */
     boolean wasRatedBy(HungryStudent student) {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        return this.students.containsKey(student);
     }
 
     /**
@@ -86,4 +115,33 @@ public class RestaurantImpl implements Restaurant {
     public Set<String> getMenu() {
         return menu;
     }
+
+
+    /**
+     * this method check equality of the object.
+     * this method should be override in subclasses
+     *
+     * @param o the other RestaurantImpl instance.
+     * @return true if they are equal, false otherwise. @see https://docs.oracle.com/javase/7/docs/api/java/lang/Object.html#equals(java.lang.Object)
+     */
+    protected boolean eq(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if (!(o instanceof RestaurantImpl)) return false;
+        RestaurantImpl other = (RestaurantImpl) o;
+        return id == other.id;
+    }
+
+    /**
+     * the method check if two RestaurantImpl are equal by comparing their id.
+     * the equals method bound to the contract in java.
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        return this.eq(o) && ((RestaurantImpl) o).eq(this);
+    }
+
 }

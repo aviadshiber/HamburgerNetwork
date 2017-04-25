9ad15c75af35fd82f0249e26e4a5b6b23681973f
Eran <eran.tzabaar@gmail.com>
2017-04-25 21:25:39 +0300

    all the functions added

--- src/OOP/Solution/HamburgerNetworkImpl.java
+++ src/OOP/Solution/HamburgerNetworkImpl.java
@@ -12,6 +12,8 @@ import OOP.Provided.Restaurant.RestaurantAlreadyInSystemException;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static OOP.Provided.Restaurant.*;
+
 /**
  * Created by ashiber on 21-Apr-17.
  */
@@ -54,20 +56,26 @@ public class HamburgerNetworkImpl implements HamburgerNetwork {
         return new ArrayList<>(restaurants.values());
     }
 
-    @Override
-    public HungryStudent getStudent(int id) throws StudentNotInSystemException {
-        validateStudentPresence(id);
-        return students.get(id);
-    }
-
     private void validateStudentPresence(int id) throws StudentNotInSystemException {
         if (!students.containsKey(id))
             throw new StudentNotInSystemException();
     }
+    private void validateRestaurantPresence(int id) throws RestaurantNotInSystemException {
+        if (!restaurants.containsKey(id))
+            throw new RestaurantNotInSystemException();
+    }
 
     @Override
-    public Restaurant getRestaurant(int id) throws Restaurant.RestaurantNotInSystemException {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    public Restaurant getRestaurant(int id) throws RestaurantNotInSystemException {
+        validateRestaurantPresence(id);
+
+        return restaurants.get(id);
+    }
+    @Override
+    public HungryStudent getStudent(int id) throws StudentNotInSystemException {
+        validateStudentPresence(id);
+
+        return students.get(id);
     }
 
     @Override
@@ -81,7 +89,13 @@ public class HamburgerNetworkImpl implements HamburgerNetwork {
 
     @Override
     public Collection<Restaurant> favoritesByRating(HungryStudent s) throws StudentNotInSystemException {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        if (s == null)
+            throw new StudentNotInSystemException();
+        int studentID = s.hashCode();
+        validateStudentPresence(studentID);
+        Set<Restaurant> result = new TreeSet<>();
+        s.getFriends().stream().sorted(Comparable::compareTo).forEachOrdered(friend -> result.addAll(friend.favoritesByDist(Integer.MIN_VALUE)));
+        return result;
     }
 
     @Override
@@ -90,20 +104,60 @@ public class HamburgerNetworkImpl implements HamburgerNetwork {
             throw new StudentNotInSystemException();
         int studentID = s.hashCode();
         validateStudentPresence(studentID);
-        List<Restaurant> result = new ArrayList<>();
-        s.getFriends().stream().sorted(Comparable::compareTo)
-                .forEachOrdered(friend ->
-                        result.addAll(friend.favoritesByDist(Integer.MAX_VALUE).stream()
-                                       .filter( r-> !result.contains(r))
-                                       .collect(Collectors.toList())
-                                    ) //end of addAll method
-                );
+        Set<Restaurant> result = new TreeSet<>();
+        s.getFriends().stream().sorted(Comparable::compareTo).forEachOrdered(friend -> result.addAll(friend.favoritesByRating(Integer.MAX_VALUE)));
         return result;
     }
 
     @Override
-    public boolean getRecommendation(HungryStudent s, Restaurant r, int t) throws StudentNotInSystemException, Restaurant.RestaurantNotInSystemException, ImpossibleConnectionException {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    public boolean getRecommendation(HungryStudent s, Restaurant r, int t) throws StudentNotInSystemException, RestaurantNotInSystemException, ImpossibleConnectionException {
+
+        Set<HungryStudent> studentsSet;
+        if(s == null)throw new StudentNotInSystemException();
+        if(r == null)throw new RestaurantNotInSystemException();
+        validateStudentPresence(s.hashCode());
+        validateRestaurantPresence(r.hashCode());
+        if(t < 0)throw new ImpossibleConnectionException();
+        if(t == 0) return s.favorites().contains(r);// if t == 0 maybe the student himself prefer the restaurant
+
+        studentsSet = new TreeSet<>();
+        studentsSet.add(s);
+        return s.getFriends().stream().anyMatch((student)->getRecommendation(student,r,t-1,studentsSet));
     }
 
+    private boolean getRecommendation(HungryStudent s, Restaurant r, int t,Set<HungryStudent> studentsSet) {
+        if(t == 0) return s.favorites().contains(r);// if t == 0 maybe the student prefer the restaurant
+        studentsSet.add(s);
+        return s.getFriends().stream().filter((student)->!studentsSet.contains(student)).anyMatch((student)->getRecommendation(student,r,t-1,studentsSet));
+    }
+
+    @Override
+    public String toString(){
+
+      /*  Registered students: <studentId1, studentId2, studentId3...>.
+     * Registered restaurants: <resId1, resId2, resId3...>.
+     * Students:
+     * <student1Id> -> [<friend1Id, friend2Id, friend3Id...>].
+     * <student2Id> -> [<friend1Id, friend2Id, friend3Id...>].
+     * ...
+     * End students.
+*/
+        String s1= "Registered students: " + students.keySet().stream().sorted().map(s->s.toString()).reduce("",(f,n)-> f.equals("")?n:f+", "+n)+".";
+        String s2= "Registered restaurants: "+ restaurants.keySet().stream().sorted().map(s->s.toString()).reduce("",(f,n)-> f.equals("")?n:f+", "+n)+".";
+        String s3 = "Students:";
+
+        StringBuilder s4 = new StringBuilder();
+
+        students.values().stream().sorted(Comparable::compareTo).forEachOrdered(
+                s-> s4.append(s.hashCode()+ " -> [" + s.getFriends().stream().map((friend)->friend.hashCode()).sorted().
+                        map((friend)->friend.toString()).reduce("",(f,n)-> f.equals("")?n:f+", "+n)+"]."
+                ).append("\n")    //end append
+        );
+
+        String s5 = "End students.";
+
+        return s1 + "\n" + s2 + "\n" + s3 + "\n"+ s4 + s5;
+    }
 }
+
+
